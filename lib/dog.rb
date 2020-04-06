2727930a5f98539c69ede4712421833f576088d5
Abel Kalum <chumbabel@gmail.com>
2020-04-06 15:01:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog
  
-attr_accessor :id, :name, :breed
- 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), ("#{value}"))}
-    self.id ||= nil
-  end
-  
+  attr_accessor :name, :breed
+  attr_reader :id
+
+   def initialize(id=nil, name, breed)
+     @id = id
+     @name = name
+     @breed = breed
+   end
+   
    def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
