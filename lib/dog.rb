8a196fbac958652a6a8a13efaf90db7bfe35f8e9
Abel Kalum <chumbabel@gmail.com>
2020-04-06 15:00:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,14 +46,12 @@ attr_accessor :id, :name, :breed
   end
   
   def self.new_from_db(row)
-    attributes_hash = {
-      :id => row[0],
-      :name => row[1],
-      :breed => row[2]
-    }
-    self.new(attributes_hash)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    dog = self.new(id, name, breed)
+    dog
   end
-  
   
   def self.find_by_name(name)
     sql = <<-SQL
